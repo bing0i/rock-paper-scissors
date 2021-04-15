497a1ca570cec38ec0e5807153b0509903e1b2cc
Courgette <courgett3@gmail.com>
2021-04-15 10:06:36 +0700

    Change the style for the legibility

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
 # Rock Paper Scissors
-This is an assignment from [The Odin Project](https://www.theodinproject.com/courses/web-development-101/lessons/rock-paper-scissors) and my first project with JavaScript. 
- Player will have fun with a computer which randomly chooses rock, paper or scissors. After 5 winning matches, the winner will be alerted. 
- Its purpose helps me put HTML/CSS and JavaScript together.
-### Screenshot
+
+This is an assignment from [The Odin Project](https://www.theodinproject.com/courses/web-development-101/lessons/rock-paper-scissors) and my first project with JavaScript.
+
+## Description
+
+Player will have fun with a computer which randomly chooses rock, paper or scissors.  
+After 5 winning matches, the winner will be alerted.
+
+## Absorbed Knowledge
+
+The project's purpose is to help me put HTML/CSS and JavaScript together.
+
+## Screenshot
+
 ![Screenshot of rock-paper-scissors](/screenshot-rock-paper-scissors.PNG)
 A screenshot of rock-paper-scissors.
-### View in browser
+
+## Live Preview
+
 You can view [Courgette's rock-paper-scissors](https://bing0i.github.io/rock-paper-scissors/) in browser.
+
 ## Author
+
 [Courgette](https://github.com/bing0i)
--- scripts.js
+++ scripts.js
@@ -1,61 +1,58 @@
 function computerPlay() {
-    let randomNumber =  Math.floor(Math.random() * Math.floor(3));
-    switch(randomNumber) {
-        case 0:
-            return 'rock';
-        case 1:
-            return 'paper';
-        case 2:
-            return 'scissors';
-        default:
-            return 'error';
-    }
+  let randomNumber = Math.floor(Math.random() * Math.floor(3));
+  switch (randomNumber) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+    default:
+      return 'error';
+  }
 }
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection)
-        return 0;
-    else if (playerSelection === 'scissors' && computerSelection === 'paper')
-        return 1;
-    else if (playerSelection === 'paper' && computerSelection === 'rock')
-        return 1;
-    else if (playerSelection === 'rock' && computerSelection === 'scissors')
-        return 1;
-    else
-        return -1;
+  if (playerSelection === computerSelection) return 0;
+  else if (playerSelection === 'scissors' && computerSelection === 'paper')
+    return 1;
+  else if (playerSelection === 'paper' && computerSelection === 'rock')
+    return 1;
+  else if (playerSelection === 'rock' && computerSelection === 'scissors')
+    return 1;
+  else return -1;
 }
 function playGame() {
-    let playerScore = 0;
-    let computerScore = 0;
-    const buttons = Array.from(document.querySelectorAll('button'));
-    buttons.forEach(button => button.addEventListener('click', function(e) {
-        let computerSelection = computerPlay();
-        let result = playRound(button.value, computerSelection);
-        pResult = document.querySelector('#showResult');
-        if (result === 1) {
-            pResult.innerText = "You win, " + button.value + " beats " + computerSelection;
-            ++playerScore;
-        }
-        else if (result === -1) {
-            pResult.innerText = "You lose, " + computerSelection + " beats " + button.value;
-            ++computerScore;
-        }
-        else
-            pResult.innerText = "Tie!";
-        pResult2 = document.querySelector('#countResult');
-        pResult2.innerText = "Hooman: " + playerScore + " Computer: " + computerScore;
-        if (playerScore === 5)
-        {
-            alert("Great, hooman!");
-            playerScore = 0;
-            computerScore = 0;
-        }
-        else if (computerScore === 5)
-        {
-            alert("Sorry, hooman!");
-            playerScore = 0;
-            computerScore = 0;
-        }
-    }));
+  let playerScore = 0;
+  let computerScore = 0;
+  const buttons = Array.from(document.querySelectorAll('button'));
+  buttons.forEach((button) =>
+    button.addEventListener('click', function (e) {
+      let computerSelection = computerPlay();
+      let result = playRound(button.value, computerSelection);
+      pResult = document.querySelector('.result');
+      if (result === 1) {
+        pResult.innerText =
+          'You win, ' + button.value + ' beats ' + computerSelection;
+        ++playerScore;
+      } else if (result === -1) {
+        pResult.innerText =
+          'You lose, ' + computerSelection + ' beats ' + button.value;
+        ++computerScore;
+      } else pResult.innerText = 'Tie!';
+      pResult2 = document.querySelector('.human-score');
+      pResult2.innerText = 'Hooman score: ' + playerScore;
+      let pResult3 = document.querySelector('.computer-score');
+      pResult3.innerText = 'Computer score: ' + computerScore;
+      if (playerScore === 5) {
+        alert('Great, hooman!');
+        playerScore = 0;
+        computerScore = 0;
+      } else if (computerScore === 5) {
+        alert('Sorry, hooman!');
+        playerScore = 0;
+        computerScore = 0;
+      }
+    })
+  );
 }
 playGame();
-
